9fc0b08f49fe4b30fea2738bb83da638c3547e82
Draugelis <viktoras.draugelis@gmail.com>
2020-06-26 07:56:53 +0300

    Added interactive mode; Fixed square overlaps

--- yeet.py
+++ yeet.py
@@ -1,18 +1,10 @@
 from PIL import Image, ImageDraw
 import sys
 
-colors = {
-    'BLACK'   : (  0,   0,   0),
-    'GRAY'    : (128, 128, 128),
-    'WHITE'   : (255, 255, 255),
-    'DGRAY'   : ( 96,  96,  96),
-    'LGRAY'   : (160, 160, 160)
-}
 
+default_args = (5, 5, 32, "output.png", "white", "gray")
 
-default_args = (5, 5, 32, "output.png", WHITE, GRAY)
-
-def draw_placeholder(w = 5, h = 5, sq_size = 32, name = "output.png", bg_color = colors["WHITE"], sq_color = colors["GRAY"]):
+def draw_placeholder(w = 5, h = 5, sq_size = 32, name = "output.png", bg_color = "white", sq_color = "black"):
     """ 
         Draw a checkerboard pattern rectancle
 
@@ -45,10 +37,10 @@ def draw_placeholder(w = 5, h = 5, sq_size = 32, name = "output.png", bg_color =
                 x0, y0  top-left corner of a square
                 x1, y1  bottom-right corner of a square
             """
-            x0 = i * sq_size
-            y0 = j * sq_size
-            x1 = (i + 1) * sq_size
-            y1 = (j + 1) * sq_size
+            x0 = i * (sq_size)
+            y0 = j * (sq_size)
+            x1 = (i + 1) * (sq_size) - 1
+            y1 = (j + 1) * (sq_size) - 1
 
             cords = [(x0, y0), (x1, y1)]
             del x0, x1, y0, y1
@@ -66,28 +58,56 @@ def draw_placeholder(w = 5, h = 5, sq_size = 32, name = "output.png", bg_color =
     image.save(name)
 
 
-if len(sys.argv) == 7:
+if sys.argv[1] == "-a" and len(sys.argv) == 8:
+    """
+        flag -a - argument mode
+
+        Input variables for draw_placeholder are passed via command line
+    """
+
+
     """
         Saving passed argument to a list 
         Skipping sys.argv[0] as it is file name
+        Skipping sys.argv[1] as it is flag
     """
-    arguments = sys.argv[1:]
+    arguments = sys.argv[2:]
 
     """
         Changing some argument types
-        This is because when I just pass args as "python3 yeet.py 16 9 32 yeet.png DGRAY GRAY"
+        This is because when I just pass args as "python3 yeet.py 16 9 32 yeet.png "white" "black"  "
         it reads every single thing as strings
     """
     arguments[0] = int(arguments[0])
     arguments[1] = int(arguments[1])
     arguments[2] = int(arguments[2])
 
-    # I don't want to talk about this part
-    arguments[4] = colors[arguments[4]]
-    arguments[5] = colors[arguments[5]]
+elif sys.argv[1] == "-i" and len(sys.argv) == 2:
+    """
+        flag -i - interactive mode
 
-else:
-    arguments = default_args
+        Input variables are entered via prompt on command line
+    """
+
+
+    """
+        This part is ugly as hell in terms of input() usage
+        However, I had to write a description for each input
+        So whatever, let it be 
+    """
+    arguments = []
+    print("Enter width in squares (w): ")
+    arguments.append(int(input()))
+    print("Enter heigth in squares (h): ")
+    arguments.append(int(input()))
+    print("Enter square side size in pixels (sq_size): ")
+    arguments.append(int(input()))
+    print("Enter output file name with file extention (name): ")
+    arguments.append(input())
+    print("Enter base RGB color (bg_color)")
+    arguments.append(input())
+    print("Enter square RGB color (sq_color)")
+    arguments.append(input())
 
 
 
